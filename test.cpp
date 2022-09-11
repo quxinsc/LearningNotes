a30e50229ce95c9d172ece0dfa9be78787201eef
quxinsc <2224721143quxin@gmail.com>
2022-09-11 19:21:27 +0800

    字符串的初始化

--- test.cpp
+++ test.cpp
@@ -1,27 +1,16 @@
 //测试程序
 #include <iostream>
-double cube(double a);
-double refcube(const double &ra);
+#include<string>
+#include<sstream>
+using namespace std;
+//初始化字符串对象
 int main ()
 {
-    using namespace std;
-    double x = 3.0;
-
-    cout << cube(x);
-    cout << " = cube of " << x << endl;
-    cout << refcube(x);
-    cout << " = cube of " << x << endl;
+    string s1;
+    string s2="Hello,C++!";
+    string s3("Hello,C++!");//c++的初始化形式
+    string s4(6,'a');//"aaaaaa"
+    cout<<s4<<endl;
     cin.get();
     return 0;
-}
-
-double cube(double a)
-{
-    a *= a * a;
-    return a;
-}
-
-double refcube(const double &ra)
-{
-    return ra*ra*ra; 
-}
+} 
