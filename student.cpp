5a8f38240c72bbd90e3ebe92d96caa119a25c133
quxinsc <2224721143quxin@gmail.com>
2022-09-12 21:29:55 +0800

    update

---
+++ student.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: quxinsc 2224721143quxin@gmail.com
+ * @Date: 2022-09-12 18:57:55
+ * @LastEditors: quxinsc 2224721143quxin@gmail.com
+ * @LastEditTime: 2022-09-12 21:08:27
+ * @FilePath: \Code\student.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include "student.h"
+#include <iostream>
+using namespace std;
+void student::Display()
+{
+    cout<<name<<' '<<age<<endl;
+    cout<<scroe<<endl;
+}     
---
+++ student.h
@@ -0,0 +1,19 @@
+/*
+ * @Author: quxinsc 2224721143quxin@gmail.com
+ * @Date: 2022-09-12 18:58:04
+ * @LastEditors: quxinsc 2224721143quxin@gmail.com
+ * @LastEditTime: 2022-09-12 19:31:51
+ * @FilePath: \Code\student.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#pragma once
+class student
+{
+public:
+    const char* name;
+    int age;
+    int scroe;
+    void Display();//没有函数体，类的声明
+};
+
+ 
