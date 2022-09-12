88bccaeabf035e61dad9e24a9331a39b66c169f6
quxinsc <2224721143quxin@gmail.com>
2022-09-12 23:33:31 +0800

    更新对象

--- student.h
+++ student.h
@@ -2,10 +2,13 @@
  * @Author: quxinsc 2224721143quxin@gmail.com
  * @Date: 2022-09-12 18:58:04
  * @LastEditors: quxinsc 2224721143quxin@gmail.com
- * @LastEditTime: 2022-09-12 19:31:51
+ * @LastEditTime: 2022-09-12 23:30:20
  * @FilePath: \Code\student.h
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by quxinsc 2224721143quxin@gmail.com, All Rights Reserved. 
  */
+
 #pragma once
 class student
 {
