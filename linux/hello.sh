6e3a334bc07aac8eb08177f28a4a81db91becdb7
quxinsc <2224721143quxin@gmail.com>
2022-10-04 12:25:03 +0800

    u

---
+++ linux/hello.sh
@@ -0,0 +1,11 @@
+#vi hello.sh
+###
+ # @Author: quxinsc 2224721143quxin@gmail.com
+ # @Date: 2022-10-04 11:40:04
+ # @LastEditors: quxinsc 2224721143quxin@gmail.com
+ # @LastEditTime: 2022-10-04 12:24:27
+ # @FilePath: \Code\linux\hello.sh
+ # @Description: 
+ # Copyright (c) 2022 by quxinsc 2224721143quxin@gmail.com, All Rights Reserved. 
+### 
+#!/bin/bash
